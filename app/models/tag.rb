c92e3a49a1db81efdc058bcd628d555d4524c410
App Academy Student <student@appacademy.io>
2013-08-23 18:01:28 -0700

    finish phase V; need to fix select box removal after posting

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,12 @@
 class Tag < ActiveRecord::Base
   attr_accessible :title
+
+  has_many :secret_taggings,
+  :class_name => "SecretTagging",
+  :foreign_key => :tag_id,
+  :primary_key => :id
+
+  has_many :secrets,
+  :through => :secret_taggings,
+  :source => :secret
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130823185419) do
+ActiveRecord::Schema.define(:version => 20130823234615) do
 
   create_table "friendships", :force => true do |t|
     t.integer  "friender_id", :null => false
@@ -23,12 +23,28 @@ ActiveRecord::Schema.define(:version => 20130823185419) do
   add_index "friendships", ["friendee_id"], :name => "index_friendships_on_friendee_id"
   add_index "friendships", ["friender_id"], :name => "index_friendships_on_friender_id"
 
+  create_table "secret_taggings", :force => true do |t|
+    t.integer  "secret_id",  :null => false
+    t.integer  "tag_id",     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "secret_taggings", ["secret_id"], :name => "index_secret_taggings_on_secret_id"
+  add_index "secret_taggings", ["tag_id"], :name => "index_secret_taggings_on_tag_id"
+
   create_table "secrets", :force => true do |t|
     t.string   "title",        :null => false
     t.integer  "author_id",    :null => false
     t.integer  "recipient_id", :null => false
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "title",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
